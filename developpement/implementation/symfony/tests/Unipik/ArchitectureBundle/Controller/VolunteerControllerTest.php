d90454f60521530e850f1539e8ff7581fb09f7f2
mcressant <michel.cressant@insa-rouen.fr>
2016-03-14 17:11:44 +0100

    skeleton de test

---
+++ developpement/implementation/symfony/tests/Unipik/ArchitectureBundle/Controller/VolunteerControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unipik\ArchitectureBundle\Controller;
+
+use Unipik\ArchitectureBundle\Controller\VolunteerController;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class VolunteerControllerTest extends WebTestCase {
+
+	public function testIndexAction() {
+		$client = static::createClient();
+
+	}
+
+}

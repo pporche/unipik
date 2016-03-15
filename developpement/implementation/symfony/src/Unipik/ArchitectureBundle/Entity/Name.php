460e9e899f765f6d442f2283d444fc49815cc361
Matthieu Martins-Baltar <matthieu.martinsbaltar@insa-rouen.fr>
2016-03-15 15:26:01 +0100

    ajouté hello worlds

---
+++ developpement/implementation/symfony/src/Unipik/ArchitectureBundle/Entity/Name.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Unipik\ArchitectureBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Name
+ *
+ * @ORM\Table(name="name")
+ * @ORM\Entity(repositoryClass="Unipik\ArchitectureBundle\Repository\NameRepository")
+ */
+class Name
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+

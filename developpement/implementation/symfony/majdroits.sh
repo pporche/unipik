460464dc930c64fa14c45538ab4a6bacd5775aca
Matthieu Martins-Baltar <matthieu.martinsbaltar@insa-rouen.fr>
2016-03-16 09:03:38 +0100

    modif droit .sh

--- developpement/implementation/symfony/majdroits.sh
+++ developpement/implementation/symfony/majdroits.sh
@@ -1,6 +1,7 @@
 chmod 755 -R ./
 chown www-data ./
 chgrp www-data -R ./
-chmod 777 -R var/logs
-chmod 777 -R var/cache
-chmod 777 -R var/session
+chmod 777 -R var/logs/
+chmod 777 -R var/cache/
+chmod 777 -R var/sessions/
+chmod +x *.sh

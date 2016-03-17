e03f5e2c305af626cbd6dc8630cc2fa065060d7c
Florian Leriche <florian.leriche@insa-rouen.fr>
2016-03-17 16:54:26 +0100

    les petits scripts de l'ambiance

--- developpement/implementation/symfony/clearcache.sh
+++ developpement/implementation/symfony/clearcache.sh
@@ -1,1 +1,8 @@
 sudo php bin/console cache:clear
+sudo chmod 755 -R ./
+sudo chown $USER -R ./
+sudo chgrp $USER -R ./
+sudo chmod 777 -R var/logs/
+sudo chmod 777 -R var/cache/
+sudo chmod 777 -R var/sessions/
+sudo chmod +x *.sh

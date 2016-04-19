eacc77238be7b5e14d3f2d54f6d9b6ea119a6d34
Florian Leriche <florian.leriche@insa-rouen.fr>
2016-04-19 12:01:27 +0200

    controller architecture

---
+++ developpement/implementation/symfony/src/Unipik/ArchitectureBundle/Controller/ArchitectureController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: florian
+ * Date: 19/04/16
+ * Time: 11:59
+ */
+
+namespace Unipik\ArchitectureBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ArchitectureController extends Controller {
+
+    public function indexAction() {
+
+    }
+
+    public function profileAction() {
+
+    }
+
+    public function plaidoyerAction() {
+
+    }
+
+    public function frimousseAction() {
+
+    }
+
+    public function mailAction() {
+
+    }
+}

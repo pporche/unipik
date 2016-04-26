6fbb2dbc1448056e621f1aed1f2b49f952742c13
florian <florian.leriche@insa-rouen.fr>
2016-04-26 15:54:09 +0200

    edit profil

--- developpement/implementation/symfony/src/Unipik/UserBundle/Controller/UserController.php
+++ developpement/implementation/symfony/src/Unipik/UserBundle/Controller/UserController.php
@@ -10,14 +10,7 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 class UserController extends Controller {
 
    public function viewProfileAction() {
-       $user = $this->getUser();
-       if (!is_object($user) || !$user instanceof UserInterface) {
-           throw new AccessDeniedException("Cet utilisateur n'a pas accès à cette section.");
-       }
 
-       return $this->render('UserBundle:Profile:show.html.twig', array(
-           'user' => $user
-       ));
     }
 
     public function editProfileAction() {

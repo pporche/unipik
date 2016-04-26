189ed7015176e98e54f16f2c84cca48e0b72be47
Matthieu Martins-Baltar <matthieu.martinsbaltar@insa-rouen.fr>
2016-04-26 10:50:20 +0200

    Réparé le formulaire de login

--- developpement/implementation/symfony/src/Unipik/UserBundle/Form/LoginType.php
+++ developpement/implementation/symfony/src/Unipik/UserBundle/Form/LoginType.php
@@ -11,6 +11,9 @@ namespace Unipik\UserBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class LoginType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
@@ -19,8 +22,10 @@ class LoginType extends AbstractType {
         ;*/
 
         $builder
-            ->add('username', null, array('label' => 'Nom d\'utilisateur'))
-            ->add('password', null, array('label' => 'Mot de passe' ))
+            ->add('username', TextType::class, array('label' => 'Nom d\'utilisateur'))
+            ->add('password', PasswordType::class, array('label' => 'Mot de passe' ))
+            ->add('remember_me', CheckboxType::class, array('label' => 'Rester connecté'))
+            ->add('submit', SubmitType::class, array('label' => 'Connexion'))
         ;
 
     }

b152f8d747c403bc5a38748d29b6aa476db21cbb
Matthieu Martins-Baltar <matthieu.martinsbaltar@insa-rouen.fr>
2016-05-13 08:41:57 +0200

    ajouté traçabilité script bash

--- developpement/implementation/symfony/addAdmin.sh
+++ developpement/implementation/symfony/addAdmin.sh
@@ -1,5 +1,8 @@
+#version 1.01, date 11/05/2016, auteur Matthieu Martins-Baltar
+#script pour ajouter un administrateur dans la base de donnée
+#(la page pour ajouter un utilisateur nécessitant d'être déjà connecté en admin)
 
-#a copier coller
+#Le script n'est actuellement pas exécutable, il faut le copier coller dans un terminal
 sudo -i -u postgres
 psql -d bdunicef -c "INSERT INTO my_user (id, username, username_canonical, email, email_canonical, enabled, salt, password, last_login, locked, expired, expires_at, confirmation_token, password_requested_at, roles, credentials_expired, credentials_expire_at, mail, nom, prenom, telFixe, telPortable) VALUES ('174', 'admin', 'admin', 'admin@admin.admin', 'admin@admin.admin', true, '3ar576dvu76soswskk8gwsks8cgkg44', '$2y$13$iUW6/KVzaux5HD0rVkpAq.MJyyKEo8XdKBdFe/DRJKobRauMal3Um', NULL, false, false, NULL, NULL, NULL, 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}', false, NULL, 'l', 'l', 'l', 'l', 'l');"
 exit

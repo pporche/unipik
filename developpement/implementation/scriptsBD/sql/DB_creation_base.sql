4dc16e6407f196fa39b4650009a8f9fa6842abf3
jpain01 <julie.pain@insa-rouen.fr>
2016-09-12 14:52:40 +0200

    scripts pour la base de données

---
+++ developpement/implementation/scriptsBD/sql/DB_creation_base.sql
@@ -0,0 +1,4 @@
+-- version 1.01, date 12/09/2016, auteur Julie Pain
+
+CREATE DATABASE bdunicef;
+CREATE USER 'unipik' WITH PASSWORD 'kaflmmppscmmbmcjpmb';
---
+++ developpement/implementation/scriptsBD/sql/DB_suppression_base.sql
@@ -0,0 +1,3 @@
+-- version 1.01, date 12/09/2016, auteur Julie Pain
+
+DROP DATABASE bdunicef;

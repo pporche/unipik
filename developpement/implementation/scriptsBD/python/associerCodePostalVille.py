4201616e7b6f8df67d485742114df8fe663541c2
mbignoux <melissa.bignoux@insa-rouen.fr>
2016-09-14 10:15:47 +0200

    ajout scripts insertion etablissement + adresse dans la BD

---
+++ developpement/implementation/scriptsBD/python/associerCodePostalVille.py
@@ -0,0 +1,23 @@
+# version 1.00, date 14/09/2016, auteur Mélissa Bignoux
+# permet de vérifier que d'associer les codes postaux aux villes
+
+import csv
+import sys
+
+def main():
+   ville = sys.argv[1].replace('-',' ')
+   ville = ville.replace('SAINTE ', 'STE ')
+   ville = ville.replace('SAINT ', 'ST ')
+   with open('../ressourcesNettoyees/laposte_hexasmal.csv', 'rt') as f:
+     reader = csv.reader(f, delimiter=',') # good point by @paco
+     for row in reader:
+          if ((row[3] == ville) or (row[4] == ville)):
+              print row[2]
+          
+
+
+ 
+main()
+
+
+
---
+++ developpement/implementation/scriptsBD/python/retirerEspace.py
@@ -0,0 +1,6 @@
+# version 1.00, date 14/09/2016, auteur Mélissa Bignoux
+# permet d'enlever les espaces dans les numéros de téléphone
+
+import sys
+
+print sys.argv[1].replace(" ","")

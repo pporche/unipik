c397244502cb031935ff73d33baaf33955d6df3f
Kyle <kafui.atanley@insa-rouen.fr>
2016-09-14 18:14:17 +0200

    Début ajout bénévol

--- developpement/implementation/symfony/src/Unipik/UserBundle/Controller/UserController.php
+++ developpement/implementation/symfony/src/Unipik/UserBundle/Controller/UserController.php
@@ -4,17 +4,33 @@ namespace Unipik\UserBundle\Controller;
 
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Controller\ProfileController as BaseController;
+use Unipik\UserBundle\Form\ProfileFormType;
 
-class UserController extends Controller {
+class UserController extends BaseController {
 
-   public function testAction() {
-        $lol = 'singe';
-        return $this->render('UserBundle::test.html.twig', array('coucou' => $lol));
-    }
 
     public function listeAction() {
         return $this->render('UserBundle::liste.html.twig');
+    }
+
+    public function modifyAction(Request $request) {
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->get('fos_user.profile.form.factory');
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->get('fos_user.user_manager');
+
+        $user = $userManager->findUserBy(array('id' => 1));
+        $form = $formFactory->createForm();
+        $form = $form->setData($user);
+
+        $form->handleRequest($request);
+
+        return $this->render('FOSUserBundle:Profile:edit.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }

ea7627043ec37e1be4811dc188cec92cd9e9b5bd
scolomies <scolomies@insa-rouen.fr>
2016-09-14 17:25:42 +0200

    subéintégration postcliptique

--- developpement/implementation/symfony/src/Unipik/UserBundle/Controller/UserController.php
+++ developpement/implementation/symfony/src/Unipik/UserBundle/Controller/UserController.php
@@ -15,6 +15,8 @@ class UserController extends Controller {
     }
 
     public function listeAction() {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('UserBundle:');
         return $this->render('UserBundle::liste.html.twig');
     }
 }
---
+++ developpement/implementation/symfony/src/Unipik/UserBundle/Repository/BenevoleRepository.php
@@ -0,0 +1,6 @@
+<?php
+use Doctrine\ORM\EntityRepository;
+
+class BenevoleRepository extends EntityRepository {
+
+}

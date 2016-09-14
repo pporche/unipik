0c331bac9c61f1b81f72488e3a33992ffb573a3d
scolomies <scolomies@insa-rouen.fr>
2016-09-14 18:16:52 +0200

    profile

--- developpement/implementation/symfony/src/Unipik/UserBundle/Controller/UserController.php
+++ developpement/implementation/symfony/src/Unipik/UserBundle/Controller/UserController.php
@@ -4,8 +4,11 @@ namespace Unipik\UserBundle\Controller;
 
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Unipik\UserBundle\Entity\Benevole;
 
 class UserController extends Controller {
 
@@ -19,5 +22,16 @@ class UserController extends Controller {
         $repository = $em->getRepository('UserBundle:Benevole');
         $listBenevoles = $repository->findAll();
         return $this->render('UserBundle::liste.html.twig', array('listBenevoles' => $listBenevoles));
+    }
+
+    public function deleteAction(Request $request) {
+        return $this->render('ArchitectureBundle::accueilBenevole.html.twig');
+    }
+
+    public function showAction($username) {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('UserBundle:Benevole');
+        $benevole = $repository->findBy(array('username' => $username));
+        return $this->render('UserBundle:Profile:showBenevole.html.twig', array('benevole' => $benevole));
     }
 }

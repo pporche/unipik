03ab22f08f54454f0f81b531670a29b9afa77bb5
Matthieu Martins-Baltar <matthieu.martinsbaltar@insa-rouen.fr>
2016-10-05 11:14:59 +0200

    debut de test pour contactType

--- developpement/implementation/symfony/tests/Unipik/Unit/ArchitectureBundle/Form/Adresse/AdresseTypeTest.php
+++ developpement/implementation/symfony/tests/Unipik/Unit/ArchitectureBundle/Form/Adresse/AdresseTypeTest.php
@@ -8,6 +8,8 @@
 
 namespace Tests\Unipik\Unit\ArchitectureBundle\Form\Adresse;
 
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Form\PreloadedExtension;
 use Tests\Unipik\Unit\ArchitectureBundle\Entity\Mocks\AdresseMock;
 use Tests\Unipik\Unit\Utils\FormTestCase;
 use Unipik\ArchitectureBundle\Form\Adresse\AdresseType;
@@ -15,6 +17,31 @@ use Unipik\ArchitectureBundle\Form\Adresse\AdresseType;
 class AdresseTypeTest extends FormTestCase {
 
     protected static $testedType = AdresseType::class;
+    protected $entityManager;
+
+    protected function getExtensions()
+    {
+        $validator  = \Symfony\Component\Validator\Validation::createValidatorBuilder()
+            ->enableAnnotationMapping()
+            ->getValidator();
+
+        // create a type instance with the mocked dependencies
+        $r = new \ReflectionClass(static::$testedType);
+        $type = $r->newInstanceArgs(array($this->entityManager));
+
+        return array(
+            new ValidatorExtension($validator),
+            new PreloadedExtension(array($type), array()),
+        );
+    }
+
+    protected function setUp()
+    {
+        // mock any dependencies
+        $this->entityManager = $this->getMockBuilder('Doctrine\Common\Persistence\ObjectManager')->getMock();
+
+        parent::setUp();
+    }
 
     public function validDataProvider()
     {
@@ -24,8 +51,8 @@ class AdresseTypeTest extends FormTestCase {
             "Adresse ville et code postal" => [
                 "Ville" => [
                     'adresse' => "22 rue du gros",
-                    'ville' => "Rouen",
-                    'codePostal' => "76000"
+                    //'ville' => "Rouen",
+                    //'codePostal' => "76000"
                 ],
                 $a1
             ],
@@ -37,30 +64,30 @@ class AdresseTypeTest extends FormTestCase {
         return [
             "Ville est null" => [
                 "Ville" => [
-                    'adresse' => [null, null, 14, "fred"],
-                    'ville' => null,
-                    'codePostal' => "76000"
+                    'adresse' => [null, null, 14, "fred"], //array of stuff instead of string
+                    //'ville' => null,
+                    //'codePostal' => "76000"
                 ]
             ],
 
             "code postal est null" => [
                 "Ville" => [
-                    'ville' => "Rouen"
+                    //'ville' => "Rouen"
                 ]
             ],
 
             "champs inexistant" => [
                 "Ville" => [
-                    'ville' => "Rouen",
-                    'codePostal' => "76000",
+                    //'ville' => "Rouen",
+                    //'codePostal' => "76000",
                     'machin' => "truc"
                 ]
             ],
 
             "Adresse est null" => [
                 "Ville" => [
-                    'ville' => "Rouen",
-                    'codePostal' => "76000"
+                    //'ville' => "Rouen",
+                    //'codePostal' => "76000"
                 ]
             ],
         ];

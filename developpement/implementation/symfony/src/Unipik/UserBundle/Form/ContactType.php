91921bca27b1813013d798d974ce57703436d46f
Kyle <kafui.atanley@insa-rouen.fr>
2016-10-11 10:04:55 +0200

    remettre le type contact

--- developpement/implementation/symfony/src/Unipik/UserBundle/Form/ContactType.php
+++ developpement/implementation/symfony/src/Unipik/UserBundle/Form/ContactType.php
@@ -26,7 +26,7 @@ class ContactType extends AbstractType {
             ->add('prenom')
             ->add('telFixe')
             ->add('telPortable')
-           /* ->add('typeContact',ChoiceType::class,array(
+            ->add('typeContact',ChoiceType::class,array(
                 'choices' => array(
                     'Étudiant' => 'etudiant',
                     'Animateur' => 'animateur',
@@ -34,7 +34,7 @@ class ContactType extends AbstractType {
                     'Autre' => 'autre'
                 )
             ))
-            ->add('respoEtablissement',CheckBoxType::class,array('label' => 'Êtes vous le chef d\'établissement ?', 'required' => false)) */
+            ->add('respoEtablissement',CheckBoxType::class,array('label' => 'Êtes vous le chef d\'établissement ?', 'required' => false))
         ;
     }
 

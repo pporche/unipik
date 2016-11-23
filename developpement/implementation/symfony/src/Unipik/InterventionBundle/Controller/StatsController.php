4bcf2bf21571a5c1677f143fa50c412edcf0386a
Florian <scolomies@insa-rouen.fr>
2016-11-23 11:49:40 +0100

    ajout stats + code moche

---
+++ developpement/implementation/symfony/src/Unipik/InterventionBundle/Controller/StatsController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scolomies
+ * Date: 23/11/16
+ * Time: 11:20
+ */
+
+namespace Unipik\InterventionBundle\Controller;
+
+
+use Symfony\Component\HttpKernel\Tests\Controller;
+
+class StatsController extends Controller {
+
+
+
+}
--- developpement/implementation/symfony/src/Unipik/InterventionBundle/Controller/VenteController.php
+++ developpement/implementation/symfony/src/Unipik/InterventionBundle/Controller/VenteController.php
@@ -17,47 +17,42 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Unipik\InterventionBundle\Entity\Vente;
 use Unipik\InterventionBundle\Form\Vente\VenteType;
-class VenteController extends Controller
-{
+class VenteController extends Controller {
 
-    public function listeAction(Request $request){
+    public function listeAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $listVente = [];
         $venteRepository = $em->getRepository('InterventionBundle:Vente');
-        if(!is_null($request->get('intervention'))){
+        if(!is_null($request->get('intervention'))) {
             $interventionRepository= $em->getRepository('InterventionBundle:Intervention');
             $intervention = $interventionRepository->find($request->get('intervention'));
             $listVente = $venteRepository->findBy(array('intervention' => $intervention));
             return $this->render('InterventionBundle:Vente:liste.html.twig',array('ventes' => $listVente));
-        }
-        else if(!is_null($request->get('etablissement'))){
+        }  else if(!is_null($request->get('etablissement'))) {
             $etablissementRepository= $em->getRepository('InterventionBundle:Etablissement');
             $etablissement = $etablissementRepository->find($request->get('etablissement'));
             $listVente = $venteRepository->findBy(array('etablissement' => $etablissement));
             return $this->render('InterventionBundle:Vente:liste.html.twig',array('ventes' => $listVente));
-        }
-        else{
+        } else {
             $listVente = $venteRepository->findAll();
             return $this->render('InterventionBundle:Vente:liste.html.twig',array('ventes' => $listVente));
         }
     }
 
-    public function consultationAction($id){
+    public function consultationAction($id) {
         $vente = $this->getDoctrine()->getManager()->getRepository("InterventionBundle:Vente")->find($id);
-
         return $this->render('InterventionBundle:Vente:consultation.html.twig',array('vente' => $vente));
     }
 
-    public function editAction($id){
+    public function editAction($id) {
         return new Response("Ici on pourra modifier la vente ".$id);
     }
 
-    public function addAction(Request $request){
+    public function addAction(Request $request) {
         $vente = new Vente();
-
         $form = $this->createForm(VenteType::class,$vente);
 
-        if($form->handleRequest($request)->isValid()){
+        if($form->handleRequest($request)->isValid()) {
             /*
              * Faire le traitement de sauvegarde  de la vente
              */
@@ -66,10 +61,10 @@ class VenteController extends Controller
 
             if(is_null($etablissement) && is_null($etablissement))
                 return new Response(" t'as rien rentré pti bout");
-            if(is_null($intervention)){
+            if(is_null($intervention)) {
                 $etablissement = $this->getDoctrine()->getManager()->getRepository('InterventionBundle:Etablissement')->find($etablissement);
                 $vente->setEtablissement($etablissement);
-            }else{
+            } else {
                 $intervention = $this->getDoctrine()->getManager()->getRepository('InterventionBundle:Intervention')->find($intervention);
                 $vente->setIntervention($intervention);
                 $vente->setEtablissement($intervention->getEtablissement());
